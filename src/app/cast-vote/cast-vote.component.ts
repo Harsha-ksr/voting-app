9cf066541f1bf99be068e09a67355c37a4c4b3c7
Harsha-ksr <105825155+Harsha-ksr@users.noreply.github.com>
2023-07-18 01:48:22 -0400

    Add files via upload

--- src/app/cast-vote/cast-vote.component.ts
+++ src/app/cast-vote/cast-vote.component.ts
@@ -8,7 +8,7 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CastVoteComponent implements OnInit {
 
-  questionsDataJson : any
+  questionsDataJson: any
   currentIndex = 0;
 
   listOfQuestions: string[] = []
@@ -23,12 +23,13 @@ export class CastVoteComponent implements OnInit {
   }]
   votingDetailsByUserTemp: any
   errorMessage?: string;
+  alreadyVoted?: string;
 
 
   constructor(private router: Router, public http: HttpClient) { }
 
   ngOnInit(): void {
-    this.http.post('http://localhost:3000/api/getQuestionsToVote', {user: this.userId}, { observe: 'response' }).subscribe({
+    this.http.post('http://localhost:3000/api/getQuestionsToVote', { user: this.userId }, { observe: 'response' }).subscribe({
       next: response => {
         const dataFromApi = JSON.stringify(response.body)
         console.log(dataFromApi)
@@ -41,51 +42,54 @@ export class CastVoteComponent implements OnInit {
       error: error => {
         this.errorMessage = error.message;
         console.error('There was an error!', error);
-     }
+      }
     })
   }
-  onItemChange(e: any){
+  onItemChange(e: any) {
     this.castedVote = e.target.value
-    console.log(" Value is : ", this.castedVote );
- }
- next() {
-  if (this.currentIndex < this.listOfQuestions.length && this.userId != null && this.castedVote != null) {
-    if(this.userId === null){
-      this.userId = ""
+    console.log(" Value is : ", this.castedVote);
+  }
+  next() {
+    if (this.currentIndex < this.listOfQuestions.length && this.userId != null && this.castedVote != null) {
+      if (this.userId === null) {
+        this.userId = ""
+      }
+      const questionObj = {
+        question: this.listOfQuestions[this.currentIndex],
+        userId: this.userId,
+        vote: this.castedVote
+      }
+      if (!(this.votingDetailsByUser.find(obj => obj.question === questionObj.question))) {
+        this.votingDetailsByUser.push(questionObj)
+      }
+      else {
+        this.alreadyVoted = "Vote already casted. Please proceed to next question."
+      }
+      console.log(questionObj)
+      console.log(this.votingDetailsByUser)
+      this.http.post('http://localhost:3000/api/castVote', { votedQuestions: questionObj }, { observe: 'response' }).subscribe({
+        next: response => {
+          if (response.status == 200) {
+            this.castVoteStatus = true
+            this.votingDetailsByUserTemp = JSON.stringify(this.votingDetailsByUser.slice(1))
+          }
+          else {
+            console.log("issue")
+            this.castVoteStatus = false
+          }
+        },
+        error: error => {
+          this.errorMessage = error.message;
+          console.error('There was an error!', error);
+        }
+      })
     }
-    const questionObj = {
-      question: this.listOfQuestions[this.currentIndex],
-      userId: this.userId,
-      vote: this.castedVote
-    }
-    if(!(this.votingDetailsByUser.find(obj => obj.question === questionObj.question))){
-      this.votingDetailsByUser.push(questionObj)
-    }
+  }
 
-    console.log(questionObj)
-    console.log(this.votingDetailsByUser)
-    this.http.post('http://localhost:3000/api/castVote', {votedQuestions: questionObj}, { observe: 'response' }).subscribe({
-      next: response => {
-        if(response.status == 200){
-          this.castVoteStatus = true
-          this.votingDetailsByUserTemp = JSON.stringify(this.votingDetailsByUser.slice(1))
-        }
-        else{
-          console.log("issue")
-          this.castVoteStatus = false
-        }
-      },
-      error: error => {
-        this.errorMessage = error.message;
-        console.error('There was an error!', error);
-    }
-  })
+  onNextQuestion() {
+    this.currentIndex++;
+    this.alreadyVoted=''
+    this.castedVote = null
   }
 }
-
-onNextQuestion(){
-  this.currentIndex++;
-  this.castedVote = null
-}
- }
 
